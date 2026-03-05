82c9c46df3015813d5fd03e7e9d18c576827ea5e
William Vance <william.vance@fhnw.edu>
2026-03-05 08:32:43 -0700

    chore: added backend and IOS tablet view

---
+++ TabletView/tabletview/tabletview/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tabletview
+//
+//  Created by William V on 3/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TabletView/tabletview/tabletview/tabletviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  tabletviewApp.swift
+//  tabletview
+//
+//  Created by William V on 3/5/26.
+//
+
+import SwiftUI
+
+@main
+struct tabletviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
