6043eca7b671014907ea590d0b3ac44ac0fe84b7
William Vance <william.vance@fhnw.edu>
2026-03-05 09:45:17 -0700

    feat: added clerk auth to our admin page

--- admin/app/layout.tsx
+++ admin/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { ClerkProvider } from "@clerk/nextjs";
+import Header from "./components/header";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +28,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        
+        <ClerkProvider>
+          <Header></Header>
+          {children}
+        </ClerkProvider>
       </body>
     </html>
   );
